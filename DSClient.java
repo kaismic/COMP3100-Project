cd463be7f81d318f1341aad9fb914e3a111e9317
Isac <isac08270@gmail.com>
2023-03-31 12:53:51 +1100

    almost completed DSClient

--- DSClient.java
+++ DSClient.java
@@ -1,4 +1,5 @@
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,7 +10,7 @@ public class DSClient {
     static final String IP_ADDRESS = "localhost";
     static final String AUTH_INFO = "isac";
 
-    static int portNum = 50000;
+    static final int portNum = 50000;
 
     static Socket socket;
     static BufferedReader reader;
@@ -17,22 +18,24 @@ public class DSClient {
     static Scanner scanner;
 
     static String largestServerType;
-    static int largestServerLimit;
-    static int curServerID = 0;
+    static int largestCoreCount = 0;
+
+    static ArrayList<Integer> serverList = new ArrayList<Integer>();
+    static int currServerIdx = 0;
 
     static void sendMessage(String msg) throws IOException {
         outStream.write((msg+"\n").getBytes());
         outStream.flush();
-        System.out.println("C SENT: " + msg);
+        // System.out.println("C SENT: " + msg);
     }
 
     static String readMessage() throws IOException {
         String msg = reader.readLine();
-        System.out.println("C RCVD: " + msg);
+        // System.out.println("C RCVD: " + msg);
         return msg;
     }
 
-    static void quitProgram() throws IOException  {
+    static void quitProgram() throws IOException {
         sendMessage("QUIT");
         readMessage();
 
@@ -42,13 +45,55 @@ public class DSClient {
         socket.close();
     }
 
+    static void getLargestServer() throws IOException {
+        // get server state information
+        sendMessage("GETS All");
+        scanner = new Scanner(readMessage());
+        scanner.next(); // skip DATA
+        int serverCount = scanner.nextInt();
+        scanner.close();
+
+        sendMessage("OK");
+
+        // gotta pick the server that comes first with the largest number of cores
+        /* name     core   memory
+         * medium    4      100
+         * large     4      200
+         */
+        // gotta pick medium not large
+
+        String serverType;
+        int coreCount;
+        int serverID;
+
+        for (int i = 0; i < serverCount; i++) {
+            scanner = new Scanner(readMessage());
+            serverType = scanner.next();
+            serverID = scanner.nextInt();
+            scanner.next(); // skip state
+            scanner.next(); // skip curStartTime;
+            coreCount = scanner.nextInt();
+
+            if (coreCount > largestCoreCount) {
+                serverList.clear();
+                serverList.add(serverID);
+                largestCoreCount = coreCount;
+                largestServerType = serverType;
+            } else if (serverType.equals(largestServerType)) {
+                serverList.add(serverID);
+            }
+            scanner.close();
+        }
+
+        sendMessage("OK");
+        readMessage(); // receive "."
+    }
+
     public static void main(String args[]) throws IOException {
         socket = new Socket(IP_ADDRESS, portNum);
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         outStream = new DataOutputStream(socket.getOutputStream());
 
-        String receivedMsg;
-        String command;
         // Handshake
         // Send HELO
         sendMessage("HELO");
@@ -61,49 +106,18 @@ public class DSClient {
         sendMessage("REDY");
 
         // Receive JOBN
-        receivedMsg = readMessage();
-        scanner = new Scanner(receivedMsg);
-        scanner.next(); // skip JOBN
+        readMessage();
 
-        scanner.nextInt(); // skip sumbitTime
-        scanner.nextInt(); // skip jobID
-        scanner.nextInt(); // skip estRunTime
-        int core = scanner.nextInt();
-        int memory = scanner.nextInt();
-        int disk = scanner.nextInt();
-        scanner.close();
+        getLargestServer();
 
-        // get server state information
-        sendMessage("GETS Capable " + core + " " + memory + " " + disk);
-        receivedMsg = readMessage();
-        scanner = new Scanner(receivedMsg);
-        scanner.next(); // skip DATA
-        int serverNum = scanner.nextInt();
-        scanner.close();
-
-        sendMessage("OK");
-        // ignore every other server info except the last one
-        for (int i = 0; i < serverNum - 1; i++) {
-            readMessage();
-        }
-        receivedMsg = readMessage(); // get last server info (largest server type)
-        scanner = new Scanner(receivedMsg);
-        largestServerType = scanner.next();
-        largestServerLimit = scanner.nextInt() + 1;
-        scanner.close();
-
-        sendMessage("OK");
-        readMessage(); // receive "."
         sendMessage("REDY");
 
+        String command;
         mainLoop: while (true) {
-            receivedMsg = readMessage();
-            scanner = new Scanner(receivedMsg);
+            scanner = new Scanner(readMessage());
             command = scanner.next();
 
             switch (command) {
-                case "ERR":
-                    break mainLoop;
                 case "NONE":
                     break mainLoop;
                 case "OK":
@@ -116,21 +130,27 @@ public class DSClient {
                     // int core = scanner.nextInt();
                     // int memory = scanner.nextInt();
                     // int disk = scanner.nextInt();
+
                     // LRR algorithm
-                    String msgToSend = "SCHD " + jobID + " " + largestServerType + " " +curServerID;
-                    curServerID = (curServerID + 1) % largestServerLimit;
+                    int serverID = serverList.get(currServerIdx);
+                    String msgToSend = "SCHD " + jobID + " " + largestServerType + " " +serverID;
+                    currServerIdx = (currServerIdx + 1) % serverList.size();
                     sendMessage(msgToSend);
                     break;
-                case "JOBP":
-                    break mainLoop;
                 case "JCPL":
                     sendMessage("REDY");
                     break;
-                case "RESF":
-                    break mainLoop;
-                case "RESR":
-                    break mainLoop;
-                case "CHKQ":
+                // case "ERR":
+                //     break mainLoop;
+                // case "JOBP":
+                //     break mainLoop;
+                // case "RESF":
+                //     break mainLoop;
+                // case "RESR":
+                //     break mainLoop;
+                // case "CHKQ":
+                //     break mainLoop;
+                default:
                     break mainLoop;
             }
             scanner.close();
