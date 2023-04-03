505bc86db0a921d628661ac22e0ff99e5159c7a8
Isac <isac08270@gmail.com>
2023-04-03 12:23:18 +1000

    fixed mistake

--- DSClient.java
+++ DSClient.java
@@ -1,6 +1,5 @@
 import java.net.Socket;
 import java.util.ArrayList;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.DataOutputStream;
@@ -79,8 +78,8 @@ public class DSClient {
             receivedMsg = readMessage().split(" ");
 
             String serverType = receivedMsg[GETS.serverType.ordinal()];
-            int coreCount = atoi(receivedMsg[GETS.serverID.ordinal()]);
-            int serverID = atoi(receivedMsg[GETS.core.ordinal()]);
+            int coreCount = atoi(receivedMsg[GETS.core.ordinal()]);
+            int serverID = atoi(receivedMsg[GETS.serverID.ordinal()]);
 
             if (coreCount > largestCoreCount) {
                 serverList.clear();
