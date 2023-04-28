b422ada7e8d93f9866aac9126f265e38fa2fb29c
Isac <isac08270@gmail.com>
2023-04-28 11:35:59 +1000

    copy and paste basic structure from stage 1

--- DSClient2.java
+++ DSClient2.java
@@ -1,3 +1,80 @@
+import java.net.Socket;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class DSClient2 {
-    
+    enum JOBN {
+        command, submitTime, jobID, estRunTime, core, memory;
+    }
+
+    enum DATA {
+        command, nRecs, recLen;
+    }
+
+    enum GETS {
+        serverType, serverID, state, curStartTime, core, memory, disk, waitingJobNum, runningJobNum;
+    }
+
+    static final String IP_ADDRESS = "localhost";
+    static final String AUTH_INFO = "isac";
+
+    static final int portNum = 50000;
+
+    static Socket socket;
+    static BufferedReader reader;
+    static DataOutputStream outStream;
+
+    static String largestServerType;
+    static int largestCoreCount = 0;
+
+    static ArrayList<Integer> serverList = new ArrayList<Integer>();
+    static int currServerIdx = 0;
+
+    static void sendMessage(String msg) throws IOException {
+        outStream.write((msg+"\n").getBytes());
+        outStream.flush();
+        // System.out.println("C SENT: " + msg);
+    }
+
+    static String readMessage() throws IOException {
+        String msg = reader.readLine();
+        // System.out.println("C RCVD: " + msg);
+        return msg;
+    }
+
+    static int atoi(String str) {
+        return Integer.parseInt(str);
+    }
+
+    static void quit() throws IOException {
+        sendMessage("QUIT");
+        readMessage();
+
+        reader.close();
+        outStream.close();
+        socket.close();
+    }
+
+
+    public static void main(String args[]) throws IOException {
+        socket = new Socket(IP_ADDRESS, portNum);
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        outStream = new DataOutputStream(socket.getOutputStream());
+
+        // Handshake
+        // Send HELO
+        sendMessage("HELO");
+        // Receive OK first time
+        readMessage();
+        sendMessage("AUTH " + AUTH_INFO);
+        // Receive OK second time
+        readMessage();
+        // Send ready message
+        sendMessage("REDY");
+
+        quit();
+    }
 }
